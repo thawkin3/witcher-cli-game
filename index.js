ada81e056f4eabcc45c54325e77261403a41fcc4
Tyler Hawkins <tylerhawkins@workfront.com>
2021-01-13 19:22:45 -0700

    feat: adds questions for weapon and night wraith

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const prompts = require('prompts');
 
+const sneakResult = Math.random() > 0.5;
+
 const questions = [
   {
     type: 'confirm',
@@ -15,6 +17,41 @@ const questions = [
       { title: 'Ciri', value: 'Ciri' },
       { title: 'Yennefer', value: 'Yennefer' },
     ],
+  },
+  {
+    type: 'select',
+    name: 'weapon',
+    message: 'Choose your weapon',
+    choices: [
+      { title: 'Silver sword', value: 'silverSword' },
+      { title: 'Dagger', value: 'dagger' },
+      { title: 'Crossbow', value: 'crossbow' },
+    ],
+  },
+  {
+    type: 'select',
+    name: 'nightWraith',
+    message: 'You stumble upon a night wraith. What do you do?',
+    choices: [
+      { title: 'Fight', value: 'fight' },
+      { title: 'Run away', value: 'runAway' },
+      { title: 'Sneak', value: 'sneak' },
+    ],
+  },
+  {
+    type: prev => prev === 'runAway' ? 'confirm' : null,
+    name: 'nightWraithRunAway',
+    message: 'You got away safely! Ready to continue on your journey?',
+  },
+  {
+    type: prev => prev === 'sneak' ? 'confirm' : null,
+    name: 'nightWraithSneak',
+    message: sneakResult ? 'You snuck by safely! Ready to continue on your journey?' : 'Oh no! It saw you! Ready to fight?',
+  },
+  {
+    type: (prev, values) => values.nightWraith === 'fight' || (values.nightWraith === 'sneak' && sneakResult === false) ? 'confirm' : null,
+    name: 'nightWraithFight',
+    message: 'Fight!',
   },
 ];
 
